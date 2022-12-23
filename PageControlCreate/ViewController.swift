f2202dc1b122854cc338f279cdaf81b7ed37dc58
Di1105 <delcioglu@gmail.com.com>
2022-12-23 10:47:10 +0300

    First Commit

--- PageControlCreate/ViewController.swift
+++ PageControlCreate/ViewController.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    //Labellar ve image eksik
+    let pageControl = UIPageControl()
+    lazy var titleLAbel = UILabel()
+    lazy var subTitleLabel = UILabel()
+    lazy var imageView = UIImageView()
+    let scrollView = UIScrollView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(pageControl)
+        view.addSubview(scrollView)
+        
+        pageControl.backgroundColor = .purple
+        pageControl.numberOfPages = 3
+        pageControl.layer.cornerRadius = 20
+        
+        scrollView.backgroundColor = .systemPink
     }
 
+    override func viewDidLayoutSubviews() {
+        setUpUI()
+        if scrollView.subviews.count == 2{
+            configureScrolView()
+        }
+    }
+    
+    func setUpUI(){
+        pageControl.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-20)
+            make.leading.equalTo(view.snp.leading).offset(10)
+            make.trailing.equalTo(view.snp.trailing).offset(-10)
+            make.height.equalTo(70)
+        }
+        
+        scrollView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-100)
+        }
+        
+    }
+    
+    func configureScrolView(){
+        scrollView.contentSize = CGSize(width: view.frame.size.width*5, height: scrollView.frame.size.height)
+        scrollView.isPagingEnabled = true
+        let colors: [UIColor] = [
+            .systemCyan,
+            .systemBlue,
+            .systemPink
+        ]
+        for x in 0..<3{
+            let page = UIView(frame: CGRect(x: CGFloat(x)*view.frame.size.width, y: 0, width: view.frame.size.width, height: scrollView.frame.size.height))
+            scrollView.addSubview(page)
+            page.backgroundColor = colors[x]
+        }
+    }
 
 }
 
